c96fd67af0e69033b7e0e4ed57fc7e4207950ec2
Falcz <lahner.m@gmail.com>
2019-10-21 08:58:59 -0300

    Updated boot_sect comments.

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,17 +1,17 @@
-  mov al, 0x07
-  mov ah, 0x07
-  int 0x10
-  mov ah, 0x0e
-  mov al, 'o'
-  int 0x10
-  mov al, 'k'
-  int 0x10
+; Set Teletype Mode
+mov ah, 0x0e
+  
+; Write 'ok'
+mov al, 'o'
+int 0x10
+mov al, 'k'
+int 0x10
 
-  ;; Infinite loop
-  jmp $
+; Infinite loop
+jmp $
 
-  ;; Padding
-  times 510-($-$$) db 0
+; Padding
+times 510-($-$$) db 0
 
-  ;; Set boot sector flag
-  dw 0xaa55
+; Set boot sector flag
+dw 0xaa55
